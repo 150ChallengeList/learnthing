63a60b9d8c7b50e979095eda0423daa74d9dcc52
v0x75 <73766242+150ChallengeList@users.noreply.github.com>
2024-08-15 14:39:47 +0200

    Update PluginProcessor.cpp

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -10,31 +10,23 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-LearnthingAudioProcessor::LearnthingAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
-#endif
+NMAudioProcessor::NMAudioProcessor()
+    : gainParameter(new juce::AudioParameterFloat("gain", "Gain", -12.0f, 12.0f, 0.0f))
 {
+    addParameter(gainParameter);
 }
 
-LearnthingAudioProcessor::~LearnthingAudioProcessor()
+NMAudioProcessor::~NMAudioProcessor()
 {
 }
 
 //==============================================================================
-const juce::String LearnthingAudioProcessor::getName() const
+const juce::String NMAudioProcessor::getName() const
 {
     return JucePlugin_Name;
 }
 
-bool LearnthingAudioProcessor::acceptsMidi() const
+bool NMAudioProcessor::acceptsMidi() const
 {
    #if JucePlugin_WantsMidiInput
     return true;
@@ -43,7 +35,7 @@ bool LearnthingAudioProcessor::acceptsMidi() const
    #endif
 }
 
-bool LearnthingAudioProcessor::producesMidi() const
+bool NMAudioProcessor::producesMidi() const
 {
    #if JucePlugin_ProducesMidiOutput
     return true;
@@ -52,7 +44,7 @@ bool LearnthingAudioProcessor::producesMidi() const
    #endif
 }
 
-bool LearnthingAudioProcessor::isMidiEffect() const
+bool NMAudioProcessor::isMidiEffect() const
 {
    #if JucePlugin_IsMidiEffect
     return true;
@@ -61,58 +53,50 @@ bool LearnthingAudioProcessor::isMidiEffect() const
    #endif
 }
 
-double LearnthingAudioProcessor::getTailLengthSeconds() const
+double NMAudioProcessor::getTailLengthSeconds() const
 {
     return 0.0;
 }
 
-int LearnthingAudioProcessor::getNumPrograms()
+int NMAudioProcessor::getNumPrograms()
 {
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
                 // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int LearnthingAudioProcessor::getCurrentProgram()
+int NMAudioProcessor::getCurrentProgram()
 {
     return 0;
 }
 
-void LearnthingAudioProcessor::setCurrentProgram (int index)
+void NMAudioProcessor::setCurrentProgram (int index)
 {
 }
 
-const juce::String LearnthingAudioProcessor::getProgramName (int index)
+const juce::String NMAudioProcessor::getProgramName (int index)
 {
     return {};
 }
 
-void LearnthingAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void NMAudioProcessor::changeProgramName (int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void LearnthingAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void NMAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    dspFilter.coefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate, 100.0f, 0.5f, 3.0f);
-
-
-    // Prepare the filter for processing
-    juce::dsp::ProcessSpec ProcessSpecThing;
-    ProcessSpecThing.sampleRate = sampleRate;
-    ProcessSpecThing.maximumBlockSize = samplesPerBlock;
-    ProcessSpecThing.numChannels = getTotalNumOutputChannels();
-
-    dspFilter.prepare(ProcessSpecThing);
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
 }
 
-void LearnthingAudioProcessor::releaseResources()
+void NMAudioProcessor::releaseResources()
 {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool LearnthingAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool NMAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
   #if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
@@ -137,44 +121,103 @@ bool LearnthingAudioProcessor::isBusesLayoutSupported (const BusesLayout& layout
 }
 #endif
 
-void LearnthingAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void NMAudioProcessor::applyLimiter(juce::AudioBuffer<float>& buffer)
+{
+    auto numChannels = buffer.getNumChannels();
+    auto numSamples = buffer.getNumSamples();
+
+    float thresholdDb = -6.0f;
+    float ceilingDb = 0.0f;
+    float linearThreshold = juce::Decibels::decibelsToGain(thresholdDb);
+    float linearCeiling = juce::Decibels::decibelsToGain(ceilingDb);
+    float sustainLevelDb = 1.0f;
+
+    float attackCoeff = std::exp(-1.0f / (attackTimeMs * getSampleRate() * 0.001f));
+    float releaseCoeff = std::exp(-1.0f / (releaseTimeMs * getSampleRate() * 0.001f));
+    float sustainLevel = juce::Decibels::decibelsToGain(sustainLevelDb);
+
+    for (int channel = 0; channel < numChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer(channel);
+
+        float gainReduction = 1.0f;
+
+        for (int sample = 0; sample < numSamples; ++sample)
+        {
+            float inputSample = channelData[sample];
+            float inputLevel = std::fabs(inputSample);
+
+            if (inputLevel > linearThreshold)
+            {
+                float targetReduction = std::max(linearThreshold / inputLevel, linearCeiling);
+                gainReduction = (inputLevel > linearThreshold)
+                    ? std::max(targetReduction, gainReduction * attackCoeff)
+                    : gainReduction * releaseCoeff;
+
+                channelData[sample] = inputSample * juce::jlimit(linearCeiling, 1.0f, gainReduction);
+            }
+            else
+            {
+                gainReduction = sustainLevel + (gainReduction - sustainLevel) * releaseCoeff;
+                channelData[sample] = inputSample * juce::jlimit(linearCeiling, 1.0f, gainReduction);
+            }
+        }
+    }
+}
+
+void NMAudioProcessor::applyGainToBuffer(juce::AudioBuffer<float>& buffer)
+{
+    auto numChannels = buffer.getNumChannels();
+    auto numSamples = buffer.getNumSamples();
+    auto dB = gainParameter->get();
+    auto gain = dBToLinear(dB);
+
+    for (int channel = 0; channel < numChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer(channel);
+        for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
+            channelData[sample] *= gain;
+    }
+}
+
+float NMAudioProcessor::dBToLinear(float dB)
+{
+    return juce::Decibels::decibelsToGain(dB);
+}
+
+void NMAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    juce::dsp::AudioBlock<float> block(buffer);
-    juce::dsp::ProcessContextReplacing<float> context(block);
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
 
-    dspFilter.process(context);
-
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-    }
+    applyLimiter(buffer);
+    applyGainToBuffer(buffer);
 }
 
 //==============================================================================
-bool LearnthingAudioProcessor::hasEditor() const
+bool NMAudioProcessor::hasEditor() const
 {
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* LearnthingAudioProcessor::createEditor()
+juce::AudioProcessorEditor* NMAudioProcessor::createEditor()
 {
-    return new LearnthingAudioProcessorEditor (*this);
+    return new NMAudioProcessorEditor (*this);
 }
 
 //==============================================================================
-void LearnthingAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void NMAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void LearnthingAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void NMAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
@@ -184,5 +227,5 @@ void LearnthingAudioProcessor::setStateInformation (const void* data, int sizeIn
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new LearnthingAudioProcessor();
+    return new NMAudioProcessor();
 }
