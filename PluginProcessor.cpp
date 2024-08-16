6195158e69534cdf890c6326ec1e1b8a4aa8e07c
v0x75 <73766242+150ChallengeList@users.noreply.github.com>
2024-08-16 13:34:22 +0200

    Update PluginProcessor.cpp

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -10,23 +10,32 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-NMAudioProcessor::NMAudioProcessor()
-    : gainParameter(new juce::AudioParameterFloat("gain", "Gain", -12.0f, 12.0f, 0.0f))
+PhatBassAudioProcessor::PhatBassAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+    : AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+        .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+        .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+    ), parameters(*this, nullptr, "PARAMETERS",
+        { std::make_unique<juce::AudioParameterFloat>("gain", "Gain", -60.0f, 24.0f, 0.0f) })
+#endif
 {
-    addParameter(gainParameter);
 }
 
-NMAudioProcessor::~NMAudioProcessor()
+PhatBassAudioProcessor::~PhatBassAudioProcessor()
 {
 }
 
 //==============================================================================
-const juce::String NMAudioProcessor::getName() const
+const juce::String PhatBassAudioProcessor::getName() const
 {
     return JucePlugin_Name;
 }
 
-bool NMAudioProcessor::acceptsMidi() const
+bool PhatBassAudioProcessor::acceptsMidi() const
 {
    #if JucePlugin_WantsMidiInput
     return true;
@@ -35,7 +44,7 @@ bool NMAudioProcessor::acceptsMidi() const
    #endif
 }
 
-bool NMAudioProcessor::producesMidi() const
+bool PhatBassAudioProcessor::producesMidi() const
 {
    #if JucePlugin_ProducesMidiOutput
     return true;
@@ -44,7 +53,7 @@ bool NMAudioProcessor::producesMidi() const
    #endif
 }
 
-bool NMAudioProcessor::isMidiEffect() const
+bool PhatBassAudioProcessor::isMidiEffect() const
 {
    #if JucePlugin_IsMidiEffect
     return true;
@@ -53,50 +62,79 @@ bool NMAudioProcessor::isMidiEffect() const
    #endif
 }
 
-double NMAudioProcessor::getTailLengthSeconds() const
+double PhatBassAudioProcessor::getTailLengthSeconds() const
 {
     return 0.0;
 }
 
-int NMAudioProcessor::getNumPrograms()
+int PhatBassAudioProcessor::getNumPrograms()
 {
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
                 // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int NMAudioProcessor::getCurrentProgram()
+int PhatBassAudioProcessor::getCurrentProgram()
 {
     return 0;
 }
 
-void NMAudioProcessor::setCurrentProgram (int index)
+void PhatBassAudioProcessor::setCurrentProgram (int index)
 {
 }
 
-const juce::String NMAudioProcessor::getProgramName (int index)
+const juce::String PhatBassAudioProcessor::getProgramName (int index)
 {
     return {};
 }
 
-void NMAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void PhatBassAudioProcessor::changeProgramName (int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void NMAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void PhatBassAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumInputChannels();
+
+    lowPassFilter.prepare(spec);
+    highPassFilter.prepare(spec);
+
+    lowPassFilter.setCutoffFrequency(120.0f);
+    highPassFilter.setCutoffFrequency(2000.0f);
+
+    lowBandCompressor.prepare(spec);
+    midBandCompressor.prepare(spec);
+    highBandCompressor.prepare(spec);
+
+    lowBandCompressor.setThreshold(juce::Decibels::decibelsToGain(-35.0f));
+    lowBandCompressor.setRatio(4.0f);
+    lowBandCompressor.setAttack(0.51f);
+    lowBandCompressor.setRelease(0.69f);
+
+    midBandCompressor.setThreshold(juce::Decibels::decibelsToGain(-35.0f));
+    midBandCompressor.setRatio(4.0f);
+    midBandCompressor.setAttack(0.40f);
+    midBandCompressor.setRelease(0.69f);
+
+    highBandCompressor.setThreshold(juce::Decibels::decibelsToGain(-35.0f));
+    highBandCompressor.setRatio(4.0f);
+    highBandCompressor.setAttack(0.33f);
+    highBandCompressor.setRelease(0.57f);
+
+    this->sampleRate = sampleRate;
 }
 
-void NMAudioProcessor::releaseResources()
+void PhatBassAudioProcessor::releaseResources()
 {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool NMAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool PhatBassAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
   #if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
@@ -121,103 +159,122 @@ bool NMAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 }
 #endif
 
-void NMAudioProcessor::applyLimiter(juce::AudioBuffer<float>& buffer)
+float PhatBassAudioProcessor::calculateEnvelope(float input)
 {
-    auto numChannels = buffer.getNumChannels();
-    auto numSamples = buffer.getNumSamples();
+    float inputLevel = fabsf(input);
+    if (inputLevel > envelope)
+        envelope = attack(inputLevel);
+    else
+        envelope = release(inputLevel);
+    return envelope;
+}
 
-    float thresholdDb = -6.0f;
-    float ceilingDb = 0.0f;
-    float linearThreshold = juce::Decibels::decibelsToGain(thresholdDb);
-    float linearCeiling = juce::Decibels::decibelsToGain(ceilingDb);
-    float sustainLevelDb = 1.0f;
+float PhatBassAudioProcessor::attack(float input)
+{
+    float attackCoeff = expf(-1.0f / (attackTime * sampleRate));
+    return attackCoeff * envelope + (1.0f - attackCoeff) * input;
+}
 
-    float attackCoeff = std::exp(-1.0f / (attackTimeMs * getSampleRate() * 0.001f));
-    float releaseCoeff = std::exp(-1.0f / (releaseTimeMs * getSampleRate() * 0.001f));
-    float sustainLevel = juce::Decibels::decibelsToGain(sustainLevelDb);
+float PhatBassAudioProcessor::release(float input)
+{
+    float releaseCoeff = expf(-1.0f / (releaseTime * sampleRate));
+    return releaseCoeff * envelope + (1.0f - releaseCoeff) * input;
+}
 
-    for (int channel = 0; channel < numChannels; ++channel)
+float PhatBassAudioProcessor::applySustain(float input)
+{
+    return sustainLevel * input;
+}
+
+float PhatBassAudioProcessor::calculateGainReduction(float envelope)
+{
+    gainReduction = 1.0f / (1.0f + envelope);
+    return gainReduction;
+}
+
+float PhatBassAudioProcessor::processSample(float inputSample)
+{
+    float env = calculateEnvelope(inputSample);
+    env = applySustain(env);
+    float gain = calculateGainReduction(env);
+    return gain * inputSample;
+}
+
+// Gain processing function
+float PhatBassAudioProcessor::applyGain(float inputSample)
+{
+    float gainFactor = juce::Decibels::decibelsToGain(gainInDecibels);
+
+    return inputSample * gainFactor;
+}
+
+void PhatBassAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear(i, 0, buffer.getNumSamples());
+
     {
+        juce::dsp::AudioBlock<float> lowBlock(buffer);
+        lowPassFilter.process(juce::dsp::ProcessContextReplacing<float>(lowBlock));
+        lowBandCompressor.process(juce::dsp::ProcessContextReplacing<float>(lowBlock));
+    }
+
+    {
+        juce::dsp::AudioBlock<float> highBlock(buffer);
+        highPassFilter.process(juce::dsp::ProcessContextReplacing<float>(highBlock));
+        highBandCompressor.process(juce::dsp::ProcessContextReplacing<float>(highBlock));
+    }
+
+    {
+        juce::dsp::AudioBlock<float> midBlock(buffer);
+        midBandCompressor.process(juce::dsp::ProcessContextReplacing<float>(midBlock));
+    }
+
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        buffer.applyGain(channel, 0, buffer.getNumSamples(), 0.18f);
+        buffer.applyGain(channel, 0, buffer.getNumSamples(), 0.59f);
+        buffer.applyGain(channel, 0, buffer.getNumSamples(), 0.70f);
+
         auto* channelData = buffer.getWritePointer(channel);
 
-        float gainReduction = 1.0f;
+        buffer.addFrom(channel, 0, lowBuffer, channel, 0, buffer.getNumSamples(), 1.00f);
 
-        for (int sample = 0; sample < numSamples; ++sample)
+        for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
         {
-            float inputSample = channelData[sample];
-            float inputLevel = std::fabs(inputSample);
+            channelData[sample] = processSample(channelData[sample]);
 
-            if (inputLevel > linearThreshold)
-            {
-                float targetReduction = std::max(linearThreshold / inputLevel, linearCeiling);
-                gainReduction = (inputLevel > linearThreshold)
-                    ? std::max(targetReduction, gainReduction * attackCoeff)
-                    : gainReduction * releaseCoeff;
+            gainInDecibels = *parameters.getRawParameterValue("gain");
 
-                channelData[sample] = inputSample * juce::jlimit(linearCeiling, 1.0f, gainReduction);
-            }
-            else
-            {
-                gainReduction = sustainLevel + (gainReduction - sustainLevel) * releaseCoeff;
-                channelData[sample] = inputSample * juce::jlimit(linearCeiling, 1.0f, gainReduction);
-            }
+            channelData[sample] = applyGain(channelData[sample]);
         }
     }
 }
 
-void NMAudioProcessor::applyGainToBuffer(juce::AudioBuffer<float>& buffer)
-{
-    auto numChannels = buffer.getNumChannels();
-    auto numSamples = buffer.getNumSamples();
-    auto dB = gainParameter->get();
-    auto gain = dBToLinear(dB);
-
-    for (int channel = 0; channel < numChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer(channel);
-        for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
-            channelData[sample] *= gain;
-    }
-}
-
-float NMAudioProcessor::dBToLinear(float dB)
-{
-    return juce::Decibels::decibelsToGain(dB);
-}
-
-void NMAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    applyLimiter(buffer);
-    applyGainToBuffer(buffer);
-}
-
 //==============================================================================
-bool NMAudioProcessor::hasEditor() const
+bool PhatBassAudioProcessor::hasEditor() const
 {
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* NMAudioProcessor::createEditor()
+juce::AudioProcessorEditor* PhatBassAudioProcessor::createEditor()
 {
-    return new NMAudioProcessorEditor (*this);
+    return new PhatBassAudioProcessorEditor (*this);
 }
 
 //==============================================================================
-void NMAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void PhatBassAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void NMAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void PhatBassAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
@@ -227,5 +284,5 @@ void NMAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new NMAudioProcessor();
+    return new PhatBassAudioProcessor();
 }
